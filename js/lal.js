6f76c73a58f57732f5080a8ea1307b3c4e285cc2
Hilal Fakhri Dzakwan <hilaldzakwan08@gmail.com>
2023-10-13 13:26:08 +0700

    chapter 3

---
+++ js/lal.js
@@ -0,0 +1,83 @@
+import { get } from "https://jscroot.github.io/api/croot.js";
+import {responseData} from "./controller/controller.js"
+import {URLGeoJson} from "./template/template.js"
+import {map} from './config/petaconfig.js';
+import {onClosePopupClick,onDeleteMarkerClick,onSubmitMarkerClick,onMapClick,onMapPointerMove,disposePopover} from './controller/popup.js';
+import {onClick} from 'https://jscroot.github.io/element/croot.js';
+import {getAllCoordinates} from './controller/cog.js';
+
+document.addEventListener("DOMContentLoaded", () => {
+    const pointTable = document.getElementById("gisTable").getElementsByTagName('tbody')[0];
+
+    fetch("waypoint.json") // Ganti "data.json" dengan nama file JSON Anda
+        .then(response => response.json())
+        .then(data => {
+            data.features.forEach(feature => {
+                if (feature.geometry.type === "Point") {
+                    const row = pointTable.insertRow();
+                    const nameCell = row.insertCell(0);
+                    const coordinatesCell = row.insertCell(1);
+                    const typeCell = row.insertCell(2);
+                    nameCell.innerText = feature.properties.name;
+                    coordinatesCell.innerText = JSON.stringify(feature.geometry.coordinates);
+                    typeCell.innerText = feature.geometry.type;
+                    
+                }
+            });
+        })
+        .catch(error => console.error("Terjadi kesalahan:", error));
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const pointTable = document.getElementById("gisTable2").getElementsByTagName('tbody')[0];
+
+    fetch("polygone.json") // Ganti "data.json" dengan nama file JSON Anda
+        .then(response => response.json())
+        .then(data => {
+            data.features.forEach(feature => {
+                if (feature.geometry.type === "Polygon") {
+                    const row = pointTable.insertRow();
+                    const nameCell = row.insertCell(0);
+                    const coordinatesCell = row.insertCell(1);
+                    const typeCell = row.insertCell(2);
+                    nameCell.innerText = feature.properties.tempat;
+                    coordinatesCell.innerText = JSON.stringify(feature.geometry.coordinates);
+                    typeCell.innerText = feature.geometry.type;
+                    
+                }
+            });
+        })
+        .catch(error => console.error("Terjadi kesalahan:", error));
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const pointTable = document.getElementById("gisTable3").getElementsByTagName('tbody')[0];
+
+    fetch("polyline.json") // Ganti "data.json" dengan nama file JSON Anda
+        .then(response => response.json())
+        .then(data => {
+            data.features.forEach(feature => {
+                if (feature.geometry.type === "LineString") {
+                    const row = pointTable.insertRow();
+                    const nameCell = row.insertCell(0);
+                    const coordinatesCell = row.insertCell(1);
+                    const typeCell = row.insertCell(2);
+                    nameCell.innerText = feature.properties.jalan;
+                    coordinatesCell.innerText = JSON.stringify(feature.geometry.coordinates);
+                    typeCell.innerText = feature.geometry.type;
+                    
+                }
+            });
+        })
+        .catch(error => console.error("Terjadi kesalahan:", error));
+});
+
+onClick('popup-closer',onClosePopupClick);
+onClick('insertmarkerbutton',onSubmitMarkerClick);
+onClick('hapusbutton',onDeleteMarkerClick);
+onClick('hitungcogbutton',getAllCoordinates);
+
+map.on('click', onMapClick);
+map.on('pointermove', onMapPointerMove);
+map.on('movestart', disposePopover);
+get(URLGeoJson,responseData);
